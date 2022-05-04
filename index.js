fb3cc88f05ac0bab8dac9c81e65aa07a3df98c0f
hamzaezzzat <hamza2007ezzat@gmail.com>
2022-05-04 18:05:57 +0200

    commit

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const mainContent = (
-    <div>
-        <header>
+function Navbar() {
+    return (
+        <nav>
             <img src="./react-logo.png" />
             <h3>React Course - Project 1</h3>
-        </header>
+        </nav>
+    );
+}
+function Body() {
+    return (
         <div className="information">
             <h1>Fun facts about React</h1>
-            <ul>
+            <ol>
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100K stars on GitHub</li>
@@ -14,11 +18,21 @@ const mainContent = (
                 <li>
                     Powers thousands of enterprise apps, including mobile apps{" "}
                 </li>
-            </ul>
+            </ol>
         </div>
-    </div>
-);
+    );
+}
+function Footer() {
+    return <footer>€‹››ﬁﬂ‡°·‚⁄€‹ Hello</footer>;
+}
+function MainContent() {
+    return (
+        <div>
+            <Navbar />
+            <Body />
+            <Footer />
+        </div>
+    );
+}
 
-ReactDOM.render(mainContent, document.getElementById("root"));
-
-// document.getElementById("root").append(JSON.stringify(mainContent));
+ReactDOM.render(<MainContent />, document.getElementById("root"));
---
+++ quiz.md
@@ -0,0 +1,22 @@
+1. Why do we need to "import React from "react" in our files?
+   Solve: To initiat react and use the framework and JSX itself
+
+2. If I were to console. log (page) in index.js, what would show up?
+   A javascript Object , [Object Object]
+
+3. What's wrong with this code:
+
+```
+const page = (
+<h1>Hello</h1›
+<p>This is my website! </p>)
+```
+
+The code must be in the one tag
+
+4. What does it mean for something to be "declarative" instead of "imperative"?
+   you tell react what you need to do, and react do it instead of you and translate it to vanilla JS
+   imerative : you do every single code in JS
+
+5. What does it mean for something to be "composable"?
+   a reusable Block of code could be use or remove
