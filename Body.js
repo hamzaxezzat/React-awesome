9bd527786672f6f0f3bb9189d297187b5a6436cf
hamzaezzzat <hamza2007ezzat@gmail.com>
2022-05-04 19:52:44 +0200

    commit

--- Body.js
+++ Body.js
@@ -1,3 +1,4 @@
+import React from "react";
 export default function Body() {
     return (
         <section className="information">
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Header from "./Header";
 import Body from "./Body";
 import Footer from "./Footer";
