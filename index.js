a0bb7305b082f8e3ba0726e23a4da4ce28b516aa
hamzaezzzat <hamza2007ezzat@gmail.com>
2022-05-04 19:35:11 +0200

    commit

--- index.js
+++ index.js
@@ -1,38 +1,11 @@
-function Navbar() {
-    return (
-        <nav>
-            <img src="./react-logo.png" />
-            <h3>React Course - Project 1</h3>
-        </nav>
-    );
-}
-function Body() {
-    return (
-        <div className="information">
-            <h1>Fun facts about React</h1>
-            <ol>
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100K stars on GitHub</li>
-                <li>Is maintained by Facebook</li>
-                <li>
-                    Powers thousands of enterprise apps, including mobile apps{" "}
-                </li>
-            </ol>
-        </div>
-    );
-}
-function Footer() {
-    return <footer>€‹››ﬁﬂ‡°·‚⁄€‹ Hello</footer>;
-}
-function MainContent() {
+function Page() {
     return (
         <div>
-            <Navbar />
+            <Header />
             <Body />
             <Footer />
         </div>
     );
 }
 
-ReactDOM.render(<MainContent />, document.getElementById("root"));
+ReactDOM.render(<Page />, document.getElementById("root"));
